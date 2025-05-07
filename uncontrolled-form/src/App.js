fcec3df7e1bac137d978b7ac62d0727816c4b8b5
Himanshu Mehta <102656780+himanshu00003@users.noreply.github.com>
2025-05-07 23:56:47 +0530

    Add files via upload

---
+++ uncontrolled-form/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UncontrolledForm from './components/UncontrolledForm';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Uncontrolled Form</h1>
+      <UncontrolledForm />
+    </div>
+  );
+}
+
+export default App;
